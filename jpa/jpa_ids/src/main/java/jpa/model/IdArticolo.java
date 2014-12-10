ba1a82bc94bc52ac4cd1e7b2db47a988b977c6c5
manuelgentile <manuelgentile@gmail.com>
2014-12-10 20:49:09 +0100

    equals hashcode

--- jpa/jpa_ids/src/main/java/jpa/model/IdArticolo.java
+++ jpa/jpa_ids/src/main/java/jpa/model/IdArticolo.java
@@ -25,4 +25,29 @@ public class IdArticolo implements Serializable {
 		this.codiceProdotto = codiceProdotto;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codiceProdotto;
+		result = prime * result + codiceProduttore;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IdArticolo other = (IdArticolo) obj;
+		if (codiceProdotto != other.codiceProdotto)
+			return false;
+		if (codiceProduttore != other.codiceProduttore)
+			return false;
+		return true;
+	}
+
 }
